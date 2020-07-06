a8d09828a47c3f5c1ca3835c97f89ba1921107f3
Reid <reid_buckingham@yahoo.com>
2020-07-05 19:40:37 -0500

    adding files

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript file?",
+        choices: ["<javascript>", "<body>", "<script>", "<js>"],
+        answer: "<script>"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function:myFunction()", "function = myFunction()", "function myFunction()"],
+        answer: "function = myFunction()"
+    },
+    {
+        title: "How to write an IF statement in JavaScript?",
+        choices: ["if i = 5 then", "if i ==5 then", "if i = 5", "if (i == 5)"],
+        answer: "if (i == 5)"
+    },
+    {
+        title: "How does a FOR loop start?",
+        choices: ["for (i <= 5; i++", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5)"],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+    {
+        title: "How can you add a comment in a JavaScript?",
+        choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment'"],
+        answer: "//This is a comment"
+    }
+]
